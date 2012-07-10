bb780c8d9fe87e7e5d56a8a8af5e66b9959ac144
kalpesh Dave <kalpesh.dave2005@gmail.com>
2012-07-10 16:18:57 +0530

    Add FB app credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'CONSUMER_KEY', 'CONSUMER_SECRET', {:scope => 'publish_stream,offline_access,email'}
+  provider :facebook, '446104495409833', 'e51291c5d0e3d76dc489ef26745b5f02', {:scope => 'publish_stream,offline_access,email'}
 #  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 #  provider :linkedin, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
